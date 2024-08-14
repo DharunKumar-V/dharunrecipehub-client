29eadabafd95076c61a646e27653a44483e376c1
DharunKumar-V <dharunkumar359@gmail.com>
2024-08-14 20:13:51 +0530

    Update login.js

--- login.js
+++ login.js
@@ -34,6 +34,19 @@ document.getElementById("userForm").addEventListener("submit",async function(eve
     
       
      });
-      
+
+ document.addEventListener('DOMContentLoaded', () => {
+  const passwordField = document.getElementById('password');
+  const showPasswordCheckbox = document.getElementById('showPassword');
+
+  showPasswordCheckbox.addEventListener('change', () => {
+      if (showPasswordCheckbox.checked) {
+          passwordField.type = 'text';
+      } else {
+          passwordField.type = 'password';
+      }
+  });
+});
+
   
   
